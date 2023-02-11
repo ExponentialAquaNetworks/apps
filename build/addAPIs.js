bbade98409f00d0a3625760c198adc0f06f71cb1
Keshavaprasad B S <kbsbng@gmail.com>
2023-02-11 20:45:43 +0530

    Auto generate API spec from swagger.jsons during build time

--- build/addAPIs.js
+++ build/addAPIs.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios");
+const async = require("async");
 const fs = require("fs");
 
 console.log("Running script to generate API functions");
@@ -10,18 +11,66 @@ async function getProjects() {
     return response.data.data;
 }
 
+const projectHandleCamelCase = {};
+async function processProject(project) {
+    console.log("Process " + project.apiDoc);
+    project.handleCamelCase = project.handle.replace(/-(\w)/g, ($0, $1) => { return $1.toUpperCase() });
+    // dedup project.handleCamelCase to ensure we don't generate two project API objects for the same project.handleCamelCase
+    if (projectHandleCamelCase[project.handleCamelCase]) {
+        project.handleCamelCase = project.handle;
+    }
+    else {
+        projectHandleCamelCase[project.handleCamelCase] = 1;
+    }
+    const response = await axios(project.apiDoc).catch((e) => {
+        throw e;
+    });
+    const paths = response.data.paths;
+    let functions = [];
+    Object.keys(paths).forEach((path) => {
+        let pathCamelCase = path.replace(/(-|\/)(\w)?/g, ($0, $1, $2) => {
+            return $2 ? $2.toUpperCase() : "";
+        });
+        let pathConfig = paths[path];
+        Object.keys(pathConfig).forEach((method) => {
+            functions.push({
+                name: method + pathCamelCase,
+                method,
+                path,
+                consumes: pathConfig[method].consumes
+            });
+        });
+    });
+    project.functions = functions;
+}
+
 async function addAPIs() {
     const projects = await getProjects();
-    projects.forEach((project) => {
-        console.log("Process " + project.apiDoc);
-        project.handleCamelCase = project.handle.replace(/-(\w)/g, ($0, $1) => { return $1.toUpperCase() });
-    });
 
-    let apisText = "module.exports = {\n";
+    try {
+        await async.eachLimit(projects, 5, processProject);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+
+    let apisText = "module.exports = function (apiCall) { \n    return {\n";
     projects.forEach((project) => {
-        apisText += `    '${project.handleCamelCase}': () => {},\n`; 
+        apisText += `        '${project.handleCamelCase}': {\n`; 
+        project.functions.forEach((func) => {
+            apisText += `            ${func.name}: (config) => {\n`;
+            if (func.consumes) {
+                if (typeof func.consumes === "object") {
+                    func.consumes = func.consumes[0];
+                }
+                apisText += `                config ||= {}; config.headers ||= {}; config.headers["content-type"] ||= "${func.consumes}";\n`;
+            }
+            apisText += `                return apiCall('${project.handle}', '${func.method}', '${func.path}', config);\n`;
+            apisText += `            },\n`;
+        });
+        apisText += '        },\n';
     });
-    apisText += "};";
+    apisText += "    }\n};";
 
     fs.writeFile(__dirname + "/../api.js", apisText, (e) => {
         if (e) {
--- index.js
+++ index.js
@@ -6,21 +6,23 @@ module.exports = function (exponentialApiKey) {
         throw 'Please pass a valid Exponential API key';
     }
 
+    async function apiCall(projectHandle, httpMethod, path, config) {
+        if ((typeof projectHandle !== 'string') || projectHandle.match(/^[a-z0-9](-?[a-z0-9])*$/) === null) {
+            throw 'Invalid project handle';
+        }
+        if ((typeof path !== 'string') || !path.startsWith('/')) {
+            throw 'path should begin with /';
+        }
+        config ||= {};
+        config.method = httpMethod;
+        config.url = 'https://' + projectHandle + '.exponential.host' + path;
+        config.headers ||= {};
+        config.headers.exponential_api_secret = exponentialApiKey;
+        return axios(config);
+    }
+
     return {
-        call: async (projectHandle, httpMethod, path, config) => { 
-            if ((typeof projectHandle !== 'string') || projectHandle.match(/^[a-z0-9](-?[a-z0-9])*$/) === null) {
-                throw 'Invalid project handle';
-            }
-            if ((typeof path !== 'string') || !path.startsWith('/')) {
-                throw 'path should begin with /';
-            }
-            config ||= {};
-            config.method = httpMethod;
-            config.url = 'https://' + projectHandle + '.exponential.host' + path;
-            config.headers ||= {};
-            config.headers.exponential_api_secret = exponentialApiKey;
-            return axios(config);
-        },
+        call: apiCall,
         credits: async () => {
             const creditsResponse = await axios({
                 method: 'GET',
@@ -33,6 +35,6 @@ module.exports = function (exponentialApiKey) {
             console.log(creditsResponse.data);
             return creditsResponse.data;
         },
-        api
+        api: api(apiCall)
     };
 }
